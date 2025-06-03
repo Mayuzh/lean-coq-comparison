e35ff1ea36c187d09287b213101724ed102b4020
Mona Zhao <70651849+Mayuzh@users.noreply.github.com>
2025-06-03 11:29:18 -0700

    set up lean

---
+++ lean/Main.lean
@@ -0,0 +1,4 @@
+import LeanCoqComp
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ lean/lakefile.lean
@@ -0,0 +1,7 @@
+import Lake
+open Lake DSL
+
+package leanCoqComp
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4" @ "master"
