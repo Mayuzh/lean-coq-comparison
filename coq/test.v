73c385e4aaacb43c79d5b75b43ed9c83b7372134
Mona Zhao <70651849+Mayuzh@users.noreply.github.com>
2025-06-04 15:59:20 -0700

    compile coq

---
+++ coq/test.v
@@ -0,0 +1,1 @@
+Theorem hello : True. Proof. exact I. Qed.
--- lean/LeanCoqComp.lean
+++ lean/LeanCoqComp.lean
@@ -1,3 +1,6 @@
--- This module serves as the root of the `LeanCoqComp` library.
--- Import modules here that should be built as part of the library.
-import LeanCoqComp.Basic
+import Mathlib.Data.Nat.Basic
+
+theorem add_zero (n : ℕ) : n + 0 = n := by
+  induction n with
+  | zero => rfl
+  | succ n ih => rw [Nat.add_succ, ih]
